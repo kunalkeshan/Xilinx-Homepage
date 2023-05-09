5f645ad0754550ffa224088e6543ba3dc60c3f75
kunalkeshan <kunalkeshan12@gmail.com>
2023-05-09 12:11:08 +0530

    chore: typography & colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#9181FF',
+        primaryLight: '#A294FF',
+        secondary: '#F8FE91',
+        textPrimary: '#373C42',
+        textSecondary: '#B7BCC2',
+      },
+      fontFamily: {
+        heading: 'Yeseva One',
+        subHeading: 'Unica One',
+        para: 'Nunito Sans',
+      }
+    },
   },
   plugins: [],
 }
