cc137848d6de3641561964791ab971a647c58dbc
Kunal Keshan <kunalkeshan12@gmail.com>
2023-05-09 15:43:01 +0530

    Feat/Navbar (#25)
    * chore: routes setup
    
    * feat: navbar completed

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,5 +1,71 @@
+import { useState } from 'react';
+import Headroom from 'react-headroom';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { RxCross2 } from 'react-icons/rx';
+import { Link } from 'react-router-dom';
+import { NAV_LINKS } from '../../data/navigation';
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	const [open, setOpen] = useState(false);
+
+	const handleMobileNavOpen = () => {
+		setOpen(!open);
+	};
+
+	return (
+		<>
+			<Headroom>
+				<nav className='w-full p-4'>
+					<div className='flex items-center justify-between max-w-7xl w-full mx-auto'>
+						<Link className='w-32 md:w-40' to='/'>
+							<img
+								src='/images/xilinx-srm-logo.png'
+								className='w-full h-auto object-contain'
+								alt='Xilinx Nexus SRM'
+							/>
+						</Link>
+						<ul className='md:flex gap-6 hidden'>
+							{NAV_LINKS.map((link, index) => (
+								<>
+									<li key={index}>
+										<Link
+											to={link.url}
+											className='underline font-semibold decoration-primaryLight underline-offset-4 text-lg hover:text-primary transition-all duration-300'
+										>
+											{link.name}
+										</Link>
+									</li>
+									<p className='text-primary'>•</p>
+								</>
+							))}
+						</ul>
+						<button
+							className='text-2xl md:hidden'
+							onClick={handleMobileNavOpen}
+						>
+							{open ? <RxCross2 /> : <GiHamburgerMenu />}
+						</button>
+					</div>
+				</nav>
+			</Headroom>
+			{open && (
+				<div className='w-full h-screen fixed top-0 left-0 bg-primaryEvenLight px-8 py-20 md:hidden'>
+					<ul className='flex flex-col gap-4'>
+						{NAV_LINKS.map((link, index) => (
+							<li key={index} onClick={() => setOpen(false)}>
+								<Link
+									to={link.url}
+									className='underline decoration-primary underline-offset-4 text-lg'
+								>
+									{link.name}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default Navbar;
