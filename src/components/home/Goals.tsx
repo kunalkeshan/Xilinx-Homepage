0f911f50b8cbb4674c25d12f4739bedec8d09b09
kunalkeshan <kunalkeshan12@gmail.com>
2023-05-11 00:05:28 +0530

    chore: added vision section in goals

--- src/components/home/Goals.tsx
+++ src/components/home/Goals.tsx
@@ -1,6 +1,30 @@
 // Includes Vision and Mission.
 const Goals = () => {
-	return <div>Goals</div>;
+	return (
+		<section className='w-full min-h-fit max-w-7xl mx-auto mt-4'>
+			{/* Vision */}
+			<div className='items-center justify-center text-center md:text-left grid grid-cols-1 md:grid-cols-2'>
+				<div>
+					<p className='font-subHeading text-base md:text-xl'>
+						A target to strive for
+					</p>
+					<h2 className='font-heading text-3xl md:text-5xl'>
+						What is our Vision?
+					</h2>
+				</div>
+				<div className='mt-4 md:mt-0'>
+					<p className='text-lg md:text-xl'>
+						The objective of the program is to enhance the
+						representation, participation, as well as
+						entrepreneurial skills and abilities of women pursuing
+						careers in the Technology field.
+					</p>
+				</div>
+			</div>
+			{/* Mission */}
+			<div></div>
+		</section>
+	);
 };
 
 export default Goals;
