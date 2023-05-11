b28f85fd0b9b7688703fd72750c5fb2b26ff894e
Kunal Keshan <kunalkeshan12@gmail.com>
2023-05-11 13:37:45 +0530

    chore: fellow members detals added

--- src/data/team.ts
+++ src/data/team.ts
@@ -5,7 +5,7 @@ interface TeamMember {
 	image: string;
 	profile: string;
 	position: string;
-	socials: {
+	socials?: {
 		website?: string;
 		linkedin?: string;
 		twitter?: string;
@@ -18,7 +18,28 @@ interface TeamCollection {
 }
 
 export const TEAM: TeamCollection = {
-	FellowMembers: [],
+	FellowMembers: [
+		{
+			name: 'Dr. S. Malarvizhi',
+			image: '/images/team/malarvizhi.jpg',
+			profile: 'https://www.srmist.edu.in/faculty/dr-s-malarvizhi/',
+			position: 'Professor of ECE',
+		},
+		{
+			name: 'Dr. Revathi Venkatraman',
+			image: '/images/team/revathi.jpg',
+			profile:
+				'https://www.srmist.edu.in/faculty/dr-revathi-venkataraman/',
+			position: 'Chairperson of School of Computing',
+		},
+		{
+			name: 'Dr. Shantanu Patil',
+			image: '/images/team/shantanu.jpg',
+			profile: 'https://www.linkedin.com/in/shantanu-patil-2355122/',
+			position:
+				'Head of Department of Translational-Medicine-And-Research and Associate Director, Innovation, Incubation & Entrepreneurship Center SRM IST',
+		},
+	],
 	Faculty: [],
 	Students: [],
 };
