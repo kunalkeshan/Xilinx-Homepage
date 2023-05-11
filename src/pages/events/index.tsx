c2dd4d2fd3677926f64ce7a9c37db0486d8b6a53
kunalkeshan <kunalkeshan12@gmail.com>
2023-05-11 19:15:55 +0530

    feat: events page

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,5 +1,20 @@
+import { EVENTS } from '../../data/events';
+import EventCard from '../../components/cards/EventCard';
+
 const EventsPage = () => {
-	return <div>EventsPage</div>;
+	return (
+		<main className='w-full px-4 py-12 max-w-7xl mx-auto text-center md:text-left'>
+			<h1 className='font-heading text-3xl md:text-7xl'>Events</h1>
+			<h2 className='font-subHeading text-xl md:text-3xl'>
+				Our impact to the community
+			</h2>
+			<section className='grid grid-cols-1 md:grid-cols-3 mt-8 gap-8'>
+				{EVENTS.map((event, index) => (
+					<EventCard key={index} data={event} />
+				))}
+			</section>
+		</main>
+	);
 };
 
 export default EventsPage;
