a17f7ca420ff1fe158c9e0852127c53d1742e9c5
kunalkeshan <kunalkeshan12@gmail.com>
2023-05-11 20:02:45 +0530

    chore: stem programme event page added

--- src/pages/events/EAndTEducation.tsx
+++ src/pages/events/EAndTEducation.tsx
@@ -1,5 +1,15 @@
 const EAndTEducationEventPage = () => {
-	return <div>EAndTEducationEventPage</div>;
+	return (
+		<main className='w-full px-4 py-12 max-w-7xl mx-auto text-center md:text-left'>
+			<h1 className='font-heading text-xl md:text-5xl'>
+				Engineering & Technology Education
+			</h1>
+			<h2 className='font-subHeading text-base md:text-xl'>
+				Preparing electronics graduates for the industry by
+				incorporating real-world applications and new technologies.
+			</h2>
+		</main>
+	);
 };
 
 export default EAndTEducationEventPage;
--- src/pages/events/PolytechnicStudents.tsx
+++ src/pages/events/PolytechnicStudents.tsx
@@ -1,5 +1,15 @@
 const PolytechnicStudentsEventsPage = () => {
-	return <div>PolytechnicStudentsEventsPage</div>;
+	return (
+		<main className='w-full px-4 py-12 max-w-7xl mx-auto text-center md:text-left'>
+			<h1 className='font-heading text-xl md:text-5xl'>
+				Polytechnic Students
+			</h1>
+			<h2 className='font-subHeading text-base md:text-xl'>
+				Equiping polytechnic students with the necessary skills and
+				knowledge for successful careers in their chosen fields.
+			</h2>
+		</main>
+	);
 };
 
 export default PolytechnicStudentsEventsPage;
