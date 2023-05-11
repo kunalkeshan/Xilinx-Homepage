639c9ce1a277b9e2a0558273585c7f08b681431f
Kunal Keshan <kunalkeshan12@gmail.com>
2023-05-11 13:58:35 +0530

    feat: member card and fellow members section

--- src/components/home/FellowMembers.tsx
+++ src/components/home/FellowMembers.tsx
@@ -4,10 +4,10 @@ import MemberCard from '../cards/MemberCard';
 const FellowMembers = () => {
 	return (
 		<section className='max-w-7xl mx-auto w-full py-8'>
-			<h2 className='font-heading text-3xl md:text-5xl'>
+			<h2 className='font-heading text-3xl md:text-5xl text-center md:text-left'>
 				Fellow Members
 			</h2>
-			<div>
+			<div className='grid grid-cols-1 md:grid-cols-3 mt-8 gap-8'>
 				{TEAM.FellowMembers.map((member, index) => (
 					<MemberCard key={index} data={member} />
 				))}
