4b4d627519121030dda55231cd1ec085fffb5e96
Kunal Keshan <kunalkeshan12@gmail.com>
2023-05-11 21:05:15 +0530

    Feat/Gallery Page (#45)

---
+++ src/data/gallery.ts
@@ -0,0 +1,29 @@
+// Gallery Data
+
+export interface Image {
+	src: string;
+	alt: string;
+}
+
+export const IMAGES: Image[] = [
+	{
+		src: '/images/gallery/1.jpeg',
+		alt: 'Education and Career avenues in Science and Technology for Women - Dr.A.Ruhan Bevi – Associate Professor / ECE',
+	},
+	{
+		src: '/images/gallery/2.jpeg',
+		alt: 'Coding with Python (in +2 curriculum) Mrs. A, Saranya, Dr. S. Kayalvizhi',
+	},
+	{
+		src: '/images/gallery/3.jpeg',
+		alt: 'Coding with Python (in +2 curriculum) Mrs. A, Saranya, Dr. S. Kayalvizhi',
+	},
+	{
+		src: '/images/gallery/4.jpeg',
+		alt: 'Faculty members of XilinxNexus team',
+	},
+	{
+		src: '/images/gallery/5.jpeg',
+		alt: 'Celebration of National Science Day 2022 on 1 st March 2022, at Orathur Panchayat Union Middle School',
+	},
+];
