3e2e28c2a6a58ce1a8852aed877af4d7271db25e
kunalkeshan <kunalkeshan12@gmail.com>
2023-05-11 20:58:33 +0530

    feat: gallery card and images

---
+++ src/components/cards/GalleryImageCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Image } from '../../data/gallery';
+
+interface GalleryImageCardProps {
+	data: Image;
+}
+
+const GalleryImageCard: React.FC<GalleryImageCardProps> = ({ data }) => {
+	return (
+		<div className='h-auto max-w-full rounded-lg overflow-hidden relative group hover:scale-125 shadow-xl hover:z-10 transition-all duration-300'>
+			<img
+				src={data.src}
+				alt={data.alt}
+				className='w-full h-auto object-contain'
+			/>
+			<p className='text-white text-center px-8 py-4 absolute -bottom-full group-hover:bottom-0 transition-all duration-300 from-tertiary to-transparent bg-gradient-to-t'>
+				{data.alt}
+			</p>
+		</div>
+	);
+};
+
+export default GalleryImageCard;
